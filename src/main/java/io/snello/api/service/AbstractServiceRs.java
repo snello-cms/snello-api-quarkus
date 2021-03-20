3958c9bc495148a28001099f16550e589a1f42a0
Andrea Montanari <monta265@gmail.com>
2021-03-20 23:19:32 +0100

    AbstractServiceRs

--- src/main/java/io/snello/api/service/AbstractServiceRs.java
+++ src/main/java/io/snello/api/service/AbstractServiceRs.java
@@ -1,12 +1,40 @@
 package io.snello.api.service;
 
 import javax.transaction.Transactional;
+import javax.validation.constraints.Null;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import io.snello.model.events.MetadataDeleteEvent;
+import io.snello.service.ApiService;
+import org.jboss.logging.Logger;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static io.snello.management.AppConstants.*;
+import static javax.ws.rs.core.Response.ok;
+import static javax.ws.rs.core.Response.serverError;
+
 public abstract class AbstractServiceRs<T, U> {
+
+    protected Logger logger = Logger.getLogger(AbstractServiceRs.class);
+
+    private ApiService apiService;
+
+    private String table;
+    private String defaultSort;
+
+    public AbstractServiceRs(ApiService apiService, String table, String defaultSort) {
+        this.apiService = apiService;
+        this.table = table;
+        this.defaultSort = defaultSort;
+    }
+
+    public AbstractServiceRs() {
+    }
 
     @POST
     @Transactional
@@ -18,8 +46,9 @@ public abstract class AbstractServiceRs<T, U> {
     @GET
     @Path("/{id}")
     @Transactional
-    public Response fetch(@PathParam("id") U id) {
-        return null;
+    public Response fetch(@PathParam("id") String id) throws Exception {
+        Map<String, Object> result = apiService.fetch(null, table, id, UUID);
+        return ok(result).build();
     }
 
 
@@ -33,16 +62,36 @@ public abstract class AbstractServiceRs<T, U> {
     @DELETE
     @Path("/{id}")
     @Transactional
-    public Response delete(@PathParam("id") U id) {
-        return null;
+    public Response delete(@PathParam("id") String id) throws Exception {
+        boolean result = apiService.delete(table, id, UUID);
+        boolean deleteFieldDefinitionsByMetadataUuid = apiService.deleteFieldDefinitionsByMetadataUuid(id);
+        if (result && deleteFieldDefinitionsByMetadataUuid) {
+            //eventPublisher.fireAsync(new MetadataDeleteEvent(id));
+            return ok().build();
+        }
+        return serverError().build();
     }
 
     @GET
-    @Transactional
-    public Response getList(
-            @DefaultValue("0") @QueryParam("startRow") Integer startRow,
-            @DefaultValue("10") @QueryParam("pageSize") Integer pageSize,
-            @QueryParam("orderBy") String orderBy, @Context UriInfo ui) {
-        return null;
+    public Response list(
+            @Null @QueryParam(SORT_PARAM) String sort,
+            @Null @QueryParam(LIMIT_PARAM) String limit,
+            @Null @QueryParam(START_PARAM) String start,
+            @Context UriInfo uriInfo) throws Exception {
+        if (sort != null)
+            logger.info(SORT_DOT_DOT + sort);
+        else
+            sort = defaultSort;
+        if (limit != null)
+            logger.info(LIMIT_DOT_DOT + limit);
+        if (start != null)
+            logger.info(START_DOT_DOT + start);
+        int l = Optional.ofNullable(limit).map(Integer::parseInt).orElse(10);
+        int s = Optional.ofNullable(start).map(Integer::parseInt).orElse(0);
+        return ok(apiService.list(table, uriInfo.getQueryParameters(), sort, l, s))
+                .header(SIZE_HEADER_PARAM, EMPTY + apiService.count(table, uriInfo))
+                .header(TOTAL_COUNT_HEADER_PARAM, EMPTY + apiService.count(table, uriInfo)).build();
     }
+
+
 }
