fbe137d00e4780c9fca928d1738cbd6b1bf92fb0
fiorenzo pizza <fiorenzino@gmail.com>
2021-03-21 22:27:08 +0100

    Content-Type: multipart/form-data; boundary=----WebKitFormBoundarywGVbOkG2S6MstZFg

--- src/main/java/io/snello/model/pojo/DocumentFormData.java
+++ src/main/java/io/snello/model/pojo/DocumentFormData.java
@@ -4,7 +4,6 @@ package io.snello.model.pojo;
 import io.snello.util.MultipartFormUtils;
 import org.jboss.resteasy.annotations.providers.multipart.PartType;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
