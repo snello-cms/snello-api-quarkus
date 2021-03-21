16bff42ef986b9e4679cfb2709266e90ede001ca
fiorenzo pizza <fiorenzino@gmail.com>
2021-03-21 22:52:25 +0100

    siamo sempre li

--- src/main/java/io/snello/model/pojo/DocumentFormData.java
+++ src/main/java/io/snello/model/pojo/DocumentFormData.java
@@ -1,17 +1,18 @@
 package io.snello.model.pojo;
 
 
-import io.snello.util.MultipartFormUtils;
 import org.jboss.resteasy.annotations.providers.multipart.PartType;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 import java.io.InputStream;
 
 public class DocumentFormData {
+
     @FormParam("file")
     @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    public byte[] bytes;
+
     public InputStream data;
 
     @FormParam("uuid")
@@ -33,18 +34,4 @@ public class DocumentFormData {
     @FormParam("table_key")
     @PartType(MediaType.TEXT_PLAIN)
     public String table_key;
-
-    public DocumentFormData(MultipartFormDataInput partFormData) {
-        try {
-            this.data = MultipartFormUtils.readFileBytesInputStream(partFormData, "file");
-            this.table_name = MultipartFormUtils.readTextField(partFormData, "table_name");
-            this.table_key = MultipartFormUtils.readTextField(partFormData, "table_key");
-            this.uuid = MultipartFormUtils.readTextField(partFormData, "uuid");
-            this.filename = MultipartFormUtils.readTextField(partFormData, "filename");
-            this.mimeType = MultipartFormUtils.readTextField(partFormData, "mimeType");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
