9884669b63e9c106b6f2e7ea325f7b63350ba74e
fiorenzo pizza <fiorenzino@gmail.com>
2026-03-08 04:32:54 +0100

    purge iiss

--- src/main/java/io/snello/service/rs/ApiServiceRs.java
+++ src/main/java/io/snello/service/rs/ApiServiceRs.java
@@ -4,6 +4,7 @@ import io.quarkus.logging.Log;
 import io.snello.model.FieldDefinition;
 import io.snello.model.Metadata;
 import io.snello.service.ApiService;
+import io.snello.util.ParamUtils;
 import io.snello.util.TableKeyUtils;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -43,6 +44,7 @@ public class ApiServiceRs {
                          @QueryParam(SORT_PARAM) String sort,
                          @QueryParam(LIMIT_PARAM) String limit,
                          @QueryParam(START_PARAM) String start) throws Exception {
+        table = ParamUtils.purgeIss(table);
         if (sort != null)
             Log.info(SORT_DOT_DOT + sort);
         if (limit != null)
@@ -64,6 +66,7 @@ public class ApiServiceRs {
     public Response fetch(@NotNull @PathParam("table") String table,
                           @NotNull @PathParam("uuid") String uuid) throws Exception {
         debug(GET.class.getName());
+        table = ParamUtils.purgeIss(table);
         String key = apiService.table_key(table);
         return ok(apiService.fetch(uriInfo.getQueryParameters(), table, uuid, key)).build();
     }
@@ -78,6 +81,7 @@ public class ApiServiceRs {
                         @Null @QueryParam(LIMIT_PARAM) String limit,
                         @Null @QueryParam(START_PARAM) String start) throws Exception {
         debug(GET.class.getName());
+        table = ParamUtils.purgeIss(table);
         if (path == null) {
             throw new Exception(MSG_PATH_IS_EMPTY);
         }
@@ -113,6 +117,7 @@ public class ApiServiceRs {
     @Path(TABLE_PATH_PARAM)
     public Response post(Map<String, Object> map,
                          @NotNull @PathParam("table") String table) throws Exception {
+        table = ParamUtils.purgeIss(table);
         Metadata metadata = apiService.metadataWithFields(table);
         String key = metadata.table_key;
         TableKeyUtils.generateUUid(map, metadata, apiService);
